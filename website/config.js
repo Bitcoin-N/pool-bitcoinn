49b82c4130fb198408f39fe137ff4ebaa4051f22
Bitcoin-N <dev@bitcoinn.biz>
2017-11-12 17:47:58 +0100

    up proyect

---
+++ website/config.js
@@ -0,0 +1,23 @@
+var api = "http://pool.bitcoinn.biz:8133";
+
+var api_blockexplorer = "http://pool.bitcoinn.biz:45223";
+
+var poolHost = "pool.bitcoinn.biz";
+
+var irc = "irc.freenode.net/#bitcoinn";
+
+var email = "support@poolhost.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "/?hash={id}#blockchain_block";
+
+var transactionExplorer = "/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/deep-gray-dark-theme.css";
+
+var networkStat = {
+    
+};
