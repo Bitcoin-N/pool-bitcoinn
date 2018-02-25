050738894d2be846dab085565692da1b39df7184
Bitcoin-N <dev@bitcoinn.biz>
2018-02-25 18:25:17 +0100

    update 2
    update oficial pool 25/02/2018

--- website/config.js
+++ website/config.js
@@ -6,9 +6,11 @@ var poolHost = "pool.bitcoinn.biz";
 
 var irc = "irc.freenode.net/#bitcoinn";
 
-var email = "support@poolhost.com";
+var email = "dev@bitcoinn.biz";
 
-var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+// var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var cryptonatorWidget = ["BTC-USD", "BTC-EUR", "BTC-RUB", "BTC-JPY"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
@@ -19,5 +21,11 @@ var transactionExplorer = "/?hash={id}#blockchain_transaction";
 var themeCss = "themes/deep-gray-dark-theme.css";
 
 var networkStat = {
-    
+    "btn": [
+        ["pool.bitcoinn.biz", "http://pool.bitcoinn.biz:8133"],
+        ["bitcoinn.criptominando.eu", "https://bitcoinn.criptominando.eu:8216"],
+        ["bitcoinn.multipool.es", "https://bitcoinn.multipool.es:8118"],
+	["btn.dreampool.info", "https://btn.dreampool.info/api"],
+	["superblockchain.con-ip.com/btn", "http://superblockchain.con-ip.com:8133"]
+    ],
 };
